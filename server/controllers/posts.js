8eb6f617def24f8cf60d72d07d872ccd352bc3a2
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2021-01-21 17:49:16 +0100

    if statement in changed - repaired 404 error

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -26,12 +26,12 @@ export const createPost = async (req, res) => {
 }
 
 export const updatePost = async (req,res) => {
-    const { id : _id } = req.params;
+    const { id: _id } = req.params;
     const post = req.body;
-    if(mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`No post with id: ${_id}`);
 
     
-    const updatedPost = PostMessage.findByIdAndUpdate(_id, { ...post, _id}, {new: true});
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id}, {new: true});
 
 
     res.json(updatedPost);
