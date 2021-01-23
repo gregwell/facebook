3ed703af8ecf525b3527d544896d62076e79305d
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2021-01-23 15:48:07 +0100

    auth form, code cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,21 @@
-import React, {useState, useEffect} from 'react';
-import { Container, AppBar, Grow, Grid, Typography } from '@material-ui/core';
-import {useDispatch} from 'react-redux';
+import React from 'react';
+import { Container} from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import {getPosts} from './actions/posts';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
-const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-
-//we have to share the state of current id within the posts and the form
-
-    return (
+const App = () => (
+    <BrowserRouter>
         <Container maxWidth="lg">
-            <AppBar className={classes.AppBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">thefacebook</Typography>
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-
-
+            <Navbar/>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/auth" exact component={Auth}/>
+            </Switch>
         </Container>
-    );
-}
+    </BrowserRouter> 
+);
 
 export default App;
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, Avatar, Typography, Toolbar, Button } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const Navbar = () => {
+const classes = useStyles();
+const user = null;
+    
+    return (
+        <AppBar className={classes.AppBar} position="static" color="inherit">
+            <div className={classes.brandContainer}>
+                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">thefacebook</Typography>
+            </div>
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                    </div>
+                ) : (
+                   <Button component={Link} to="/auth" variant="contained" color="primary" >Sing in</Button>
+                )}
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
